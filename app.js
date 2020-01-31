718982205cf20494d3fc5ae5af895038eb65f154
Rui <3116049c@icloud.com>
2020-02-01 00:37:42 +0900

    port change

--- app.js
+++ app.js
@@ -38,4 +38,4 @@ app.use("/intermediateQuiz", require("./routes/intermediateQuiz"))
 app.use("/answerResult", require("./routes/answerResult"))
 app.use("/postQuiz", require("./routes/postQuiz"))
 
-app.listen(9001)
+app.listen(process.env.PORT)
